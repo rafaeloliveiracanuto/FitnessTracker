d6ad9f3addd4810a38daa719a406efdd1b5c19d6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-10-10 17:37:38 -0300

    Recognize button

---
+++ app/src/main/java/com/example/fitnesstracker/ImcActivity.java
@@ -0,0 +1,13 @@
+package com.example.fitnesstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.fitnesstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View buttonImc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonImc = findViewById(R.id.button_imc);
+
+        buttonImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
