5f993d1907c2fa71515e155d5fd2994fdce21014
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-10-10 17:48:55 -0300

    Add intent to go to the ImcActivity

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         buttonImc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+                startActivity(intent);
             }
         });
     }
